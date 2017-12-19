cd59ab69b78f9209f1917aa51cac4f5b144e4c05
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-19 15:42:09 -0600

    test api call

--- routes/auth.js
+++ routes/auth.js
@@ -2,27 +2,24 @@ const express = require('express');
 var request = require("request");
 const router = express.Router();
 
-var options = { method: 'POST',
-url: 'https://pinterest-clone.auth0.com/oauth/token',
-headers: { 'content-type': 'application/json' },
-body: 
- { grant_type: 'client_credentials',
-   client_id: process.env.CLIENT_ID,
-   client_secret: process.env.CLIENT_SECRET,
-   audience: 'https://pinterest-clone.auth0.com/api/v2/' },
-json: true };
 
+router.post('/user-info', function (req, res) {
+  //res.send(JSON.stringify({ a: 1 }, null, 3));
 
-router.post('/user-info', function(req, res){
-    //res.send(JSON.stringify({ a: 1 }, null, 3));
+  var options = {
+    method: 'POST',
+    url: 'https://pinterest-clone.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{"client_id": ${process.env.CLIENT_ID},"client_secret": ${process.env.CLIENT_SECRET},"audience":"https://pinterest-clone.auth0.com/api/v2/","grant_type":"client_credentials"}'
+  };
 
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-    
-      console.log(body);
-      res.send(JSON.stringify(body));
-    });
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+    res.send(JSON.stringify(body))
   });
+
+});
 
 
 module.exports = router;
