c9067386efb848c491a05748c98a65b6f7783b88
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-20 12:16:22 -0600

    Test

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,6 @@ const router = express.Router();
 router.post('/user-info', function (req, res) {
   //res.send(JSON.stringify({ a: 1 }, null, 3));
 
-  const str = '{"client_id":"' + process.env.CLIENT_ID + '","client_secret":"'+ process.env.CLIENT_SECRET +'","audience":"https://pinterest-clone.auth0.com/api/v2/","grant_type":"client_credentials"}';
-  console.log(str);
-
   let options = { method: 'POST',
   url:  process.env.URL,
   headers: { 'content-type': 'application/json' },
