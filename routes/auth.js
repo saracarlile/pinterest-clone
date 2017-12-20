ba6284d0f214c2ba542ea0bc0f2d1ed983a92247
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-20 11:31:35 -0600

    test

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.post('/user-info', function (req, res) {
   //res.send(JSON.stringify({ a: 1 }, null, 3));
 
-  var options = { method: 'POST',
+  let options = { method: 'POST',
   url: 'https://pinterest-clone.auth0.com/oauth/token',
   headers: { 'content-type': 'application/json' },
-  body: '{"client_id":"gPcYTwW26XC4uQavEXgp2OHzvHLU4vEv","client_secret":"wnnzWkOl2-MxGT76sdGYWB-ThOr8B2uWJ16TFtnvBPo2f0js02of-IO5O9ZwO0xS","audience":"https://pinterest-clone.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+  body: '{"client_id":' +  process.env.CLIENT_ID + ',"client_secret":' + process.env.CLIENT_SECRET + ',"audience":"https://pinterest-clone.auth0.com/api/v2/","grant_type":"client_credentials"}' };
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
