b29528802c8d97394eb47f5795caa207a9c0e862
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-20 12:32:13 -0600

    extract access token from response body

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,10 @@ const router = express.Router();
 
 
 router.post('/user-info', function (req, res) {
-  //res.send(JSON.stringify({ a: 1 }, null, 3));
-
 
   let options = { method: 'POST',
   url:  process.env.URL,
   headers: { 'content-type': 'application/json' },
-  // body: '{"client_id":"gPcYTwW26XC4uQavEXgp2OHzvHLU4vEv","client_secret":"wnnzWkOl2-MxGT76sdGYWB-ThOr8B2uWJ16TFtnvBPo2f0js02of-IO5O9ZwO0xS","audience":"https://pinterest-clone.auth0.com/api/v2/","grant_type":"client_credentials"}' };
   body: process.env.BODY };
   
 
@@ -19,7 +16,7 @@ request(options, function (error, response, body) {
   if (error) throw new Error(error);
 
   res.send(body);
-  console.log(body);
+  console.log(body.access_token);
 });
 
 });
