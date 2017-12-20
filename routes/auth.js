f2afe6d7fa9fc6417b269e4d1adc775e3978e7cb
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-20 12:42:06 -0600

    test

--- routes/auth.js
+++ routes/auth.js
@@ -5,19 +5,39 @@ const router = express.Router();
 
 router.post('/user-info', function (req, res) {
 
-  let options = { method: 'POST',
-  url:  process.env.URL,
-  headers: { 'content-type': 'application/json' },
-  body: process.env.BODY };
-  
+  let id = req.body.Id;
+
+  let options = {
+    method: 'POST',
+    url: process.env.URL,
+    headers: { 'content-type': 'application/json' },
+    body: process.env.BODY
+  };
 
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
 
-  res.send(body);
-  console.log(body.access_token);
-});
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+
+    console.log(body.access_token);
+    let token = body.access_token;
+
+
+    let apioptions = {
+      method: 'GET',
+      url:'https://pinterest-clone.auth0.com/api/v2/users/' + id,
+      headers: { authorization: 'Bearer ' + token }
+    };
+
+    request(apioptions, function (error, response, body) {
+      if (error) throw new Error(error);
+      res.send(body);
+      console.log(body);
+    });
+
+
+  });
 
 });
 
