d9702a5552e009e55a777783443f370b4f0b94a6
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-20 11:01:03 -0600

    add json stringify

--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,17 @@ const router = express.Router();
 router.post('/user-info', function (req, res) {
   //res.send(JSON.stringify({ a: 1 }, null, 3));
 
-  var options = { method: 'POST',
-  url: 'https://pinterest-clone.auth0.com/oauth/token',
-  headers: { 'content-type': 'application/json' },
-  body:   {
+  let body = JSON.stringify({
     "client_id": process.env.CLIENT_ID,
     "client_secret": process.env.CLIENT_SECRET,
     "audience": "https://pinterest-clone.auth0.com/api/v2/",
     "grant_type": "client_credentials"
-  }, 
+  });
+
+  let options = { method: 'POST',
+  url: 'https://pinterest-clone.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body:  body, 
   json: true };
 
 request(options, function (error, response, body) {
