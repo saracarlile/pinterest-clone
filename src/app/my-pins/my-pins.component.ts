5004d78e20161f479268e0c0eed1103ac6a6e86e
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-12-21 14:51:15 -0600

    test my pins client side

--- src/app/my-pins/my-pins.component.ts
+++ src/app/my-pins/my-pins.component.ts
@@ -12,9 +12,27 @@ export class MyPinsComponent implements OnInit {
 
   constructor(public pins: PinsService) { }
 
-  public addPin(): void {
+  public hideAdd = false;
+  public pinUrl = '';  // pin url from input box
+  public pinName= '';  //pin url from input box
+  public myPins = [];
+
+  public showAddPin(): void {
+    this.hideAdd = true;
     console.log('Add Pin')!
   } 
+
+  public addPin(): void {
+    let name = this.pinName;
+    let url = this.pinUrl;
+
+    this.myPins.push({"name": this.pinName, "url" : this.pinUrl})
+    
+    this.pinUrl = '';
+    this.pinName = '';
+    this.hideAdd = false;
+
+  }
 
   ngOnInit() {
   }
